988999fb931abca318521ae91df2e3f6cd38d00f
Vansh <tsd2405@gmail.com>
2021-05-31 10:28:45 +0000

    Done

---
+++ assets/details.js
@@ -0,0 +1,7 @@
+
+export const Header = {
+    logo: {
+        src: "https://archive.org/download/amazon-2/amazon_PNG11.png",
+        alt: "Amazon Logo"
+    },
+}
---
+++ components/TheHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex bg-amazon_blue p-1 flex-grow py-2">
+    <div class="mt-2 flex items-center flex-grow sm:flex-grow-0">
+      <img
+        width="150"
+        height="40"
+        class="object-contain cursor-pointer"
+        :src="src"
+        :alt="alt"
+      />
+    </div>
+    <div class="flex h-8">
+      <input type="text" />
+      <SearchIcon class="h-4 p-4 text-white" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { Header } from "@/assets/details";
+import { SearchIcon } from "@vue-hero-icons/outline";
+
+export default {
+  components: { SearchIcon },
+  data() {
+    let logo = Header.logo;
+    return { ...logo };
+  },
+};
+</script>
+
+<style>
+</style>
