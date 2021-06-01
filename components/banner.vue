028cebeae9f74c5c3eb4f3148da5bd9d71f78110
Vansh <tsd2405@gmail.com>
2021-06-01 15:31:43 +0000

    Add

--- components/banner.vue
+++ components/banner.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-<VueSlickCarousel v-bind="settings">
+    <VueSlickCarousel :settings="settings">
       <div>1</div>
       <div>2</div>
       <div>3</div>
@@ -14,45 +14,11 @@ import VueSlickCarousel from "vue-slick-carousel";
 import "vue-slick-carousel/dist/vue-slick-carousel.css";
 // optional style for arrows & dots
 import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
-
+import { settings } from "@/assets/details";
 export default {
-  name: "Carousal",
   components: { VueSlickCarousel },
   data() {
-    const settings = {
-      dots: true,
-      infinite: false,
-      speed: 500,
-      slidesToShow: 4,
-      slidesToScroll: 4,
-      initialSlide: 0,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true,
-          },
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            initialSlide: 2,
-          },
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-          },
-        },
-      ],
-    };
+    return { settings };
   },
 };
 </script> 
